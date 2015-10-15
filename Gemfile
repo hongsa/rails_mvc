64e19a2e255d74b1ba37f7e4556aaa35c0e43b83
hongsa <hsasung@gmail.com>
2015-10-15 16:05:15 +0900

    이메일 프로그램 완성

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+
+#google email gem
+gem "gmail"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
   end
 
   def email_send
-    @email = params[:email]
-    @content = params[:content]
+    email = params[:email]
+    title = params[:title]
+    content = params[:content]
+
+    gmail = Gmail.connect("hsasung@gmail.com", "132456ghd")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+
+
   end
 end
